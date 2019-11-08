3cb7fc60e3e5114260bbf96d922fb5b815778db0
alopez00Git <contact.alopez@gmail.com>
2019-11-07 22:23:59 -0500

    repushing lab8_doOver

--- lab_8/server.js
+++ lab_8/server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const fetch = require("node-fetch");
+const express = require('express');
+const fetch = require('node-fetch');
 
 const app = express();
 const port = 3000;
@@ -35,60 +35,23 @@ app.use(express.json());
  *  - Express: app.use()
  *    https://expressjs.com/en/4x/api.html#app.use
  */
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // this is a single route, in the simplest possible format
 // the simplest format is not necessarily the best one.
 // this is, right now, an introduction to Callback Hell
 // but it is okay for a first-level example
-app.get("/api", (req, res) => {
-  const baseURL = "https://api.umd.io/v0/courses?dept_id=INST";
-  var count = 0;
+app.get('/api', (req, res) => {
+  const baseURL = 'https://api.umd.io/v0/bus/routes';
   fetch(baseURL)
-    .then(res => res.json())
-
-    .then(res => res.map(r => r))
-
-    .then(r => {
-      text2 = "<ul>";
-      for (i = 0; i < r.length; i++) {
-        //concatinating route title and id as list items
-        text2 += "<li> " + r[i].course_id + " : " + r[i].name + " </li>";
-      } //END for loop
-      text2 += "</ul>";
-      document.querySelector(".content").innerHTML = text2;
-      return r;
-    }) //end then
-
-    //.then(data => (document.querySelector(".content").innerHTML = data));
-
-    .then(res => {
-      var x = document.getElementsByClassName("header");
-
-      //creating a random number
-      var randoNumber = Math.floor(Math.random() * res.length);
-      var rTitle = res[randoNumber].name;
-      var rID = res[randoNumber].course_id;
-
-      function checkMatch(arg1, arg2) {
-        if (arg1.search(arg2) == -1) {
-          return arg1;
-        } else if (arg1.match(rID).length >= 1) {
-          arg1 = arg1.replace(rID, "");
-          return arg1;
-        }
-      }
-
-      rTitle = checkMatch(rTitle, rID);
-
-      x[0].innerHTML = "UMD Bus Route " + rID + " " + rTitle;
-
-      console.log("response: ", res);
+    .then((r) => r.json())
+    .then((data) => {
+      console.log(data);
+      res.send({ data: data });
     })
-
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
-      res.redirect("/error");
+      res.redirect('/error');
     });
 });
 
